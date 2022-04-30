493ae93d560dc5a5d5b8fab719d2ff29f2f4d5ca
ManzTzy1 <aurelcangtip07@gmail.com>
2022-05-01 05:42:00 +0700

    mdl

---
+++ command/config.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//━━━━━━━━━━━━━━━[ WEBSITE API ]━━━━━━━━━━━━━━━━━//
+
+global.APIs = {
+zeroyt7: 'https://zeroyt7-api.xyz',
+}
+
+//━━━━━━━━━━━━━━━[ APIKEY WEBSITE API ]━━━━━━━━━━━━━━━━━//
+
+global.APIKeys = {
+'https://zeroyt7-api.xyz': '5FCB423FC44A',
+}
+
+//━━━━━━━━━━━━━━━[ OTHER ]━━━━━━━━━━━━━━━━━//
+
+global.ownername = '𝑴𝒂𝒏𝒛 𝑻𝒛𝒚'
+global.owner = ['6281327496283']
+global.packname = 'Ｎａｋａｎｏ - ＭＤ'
+global.author = 'Ｎａｋａｎｏ - ＭＤ'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+admin: 'Fitur Khusus Admin Group!',
+botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+owner: 'Fitur Khusus Owner Bot',
+eror: 'Yah eror',
+group: 'Fitur Digunakan Hanya Untuk Group!',
+private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+bot: 'Fitur Khusus Pengguna Nomor Bot',
+wait: 'Loading...',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
